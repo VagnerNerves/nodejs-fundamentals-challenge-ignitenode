24f36e9d8d99ae56e2cf6c3def7f7195fd90760d
Vagner Nerves <vagnernervessantos@gmail.com>
2023-09-05 13:38:04 -0300

    feat: Created routes.

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import { Database } from "./database.js";
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: "GET",
+    path: "/tasks",
+    handler: (req, res) => {
+      const tasks = database.select("tasks");
+
+      return res.end(JSON.stringify(tasks));
+    },
+  },
+];
